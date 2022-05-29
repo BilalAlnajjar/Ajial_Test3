37e24637b0c0f4021d4603a820518f1b2bedd2aa
BilalAlnajjar <bilalalnajjar46@gmail.com>
2022-05-30 01:37:03 +0300

    add hero section

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # To Gettin Started with Task 3 must 
 
+
 ### 'npm install'
 
 # Getting Started with Create React App
@@ -11,6 +12,7 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 In the project directory, you can run:
 
 ### `npm start`
+
 
 Runs the app in the development mode.\
 Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
---
+++ src/Component/HeroSection.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "../css/style.css"
+import Image from "../download.jpeg"
+
+class HeroSection extends React.Component{
+    render() {
+        return (
+            <div className="hero">
+                <div className="heroBackground">
+                <div className="backgroundBalk">
+
+                </div>
+                <div className="backgroundImage">
+
+                    <img src={Image} alt="photo"></img>
+                </div>
+                <div className="backgroundWhite">
+
+                </div>
+                </div>
+            <section className="heroSection">
+                <div className="heroTitle">
+                    <h1>MODERN<span>I</span></h1>
+                    <h1>ART GAL<span>LERY</span></h1>
+                </div>
+                <div className="heroDescription">
+                    <p>Lorem Ipsum is simply dummy text of the printing and
+                        typesetting industry. Lorem Ipsum has been the industry's
+                        standard dummy text ever since the 1500s, when an unknown printer
+
+                    </p>
+                    <div>
+                        <button className="ourBtn">OUR LOCATION</button>
+                        <button className="rightBtn"> > </button>
+                    </div>
+
+                </div>
+            </section>
+            </div>
+        )
+    }
+}
+
+export default HeroSection;
