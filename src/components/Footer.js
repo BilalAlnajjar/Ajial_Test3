3c9da26ff621c8a94aa8aebf59e797b4beacc940
EmanHass <eman_hassouna@hotmail.com>
2022-05-30 20:46:08 +0300

    create-location-and-footer

---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+import React from "react";
+// import '../App.css';
+class Footer extends React.Component{
+  render(){
+    return (
+      <footer className="gallery">
+          <div className="gall-head">
+              <h1>MODERN ART GALLERY</h1>
+          </div>
+
+          <div className="gall-content">
+              <p>jngggggbnruigjufgfffffffffffffffffffffferiieuoruoerjeifcnejkvgn</p>
+          </div>
+
+          <div className="gall-icons">
+              <i className="">fecebook</i>
+          </div>
+      </footer>
+    ); 
+  }
+}
+
+export default Footer;
---
+++ src/components/LocationSection.js
@@ -0,0 +1,27 @@
+import React from "react";
+// import '../App.css';
+class LocationSection extends React.Component{
+  render(){
+    return (
+      <div className="location">
+          <div className="header">
+              <h1>OUR <br></br> LOCATION</h1>
+          </div>
+
+          <div className="street">
+              <h1>99 KING STREET</h1>
+              <h6>Newport</h6>
+              <h6>RI 02840</h6>
+              <h6>United states of America</h6>
+
+              <p>Our jhjvvvvvvvvvvvvvoiweodjde8ruerwehdjehlkoijkdfjhuerlfjkefhurdjfhaswwh<br></br>
+              Our jhjvvvvvvvvvvvvvoiweodjde8ruerwehdjehlkoijkdfjhuerlfjkefhurdjfhaswwh<br></br>
+              Our jhjvvvvvvvvvvvvvoiweodjde8ruerwehdjehlkoijkdfjhuerlfjkefhurdjfhaswwh<br></br>
+              </p>
+          </div>
+      </div>
+    ); 
+  }
+}
+
+export default LocationSection;
