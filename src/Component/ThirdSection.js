ac910c3486a6c20635a2209ee7810d15768925b2
AmiraAliJa1282001 <aa.mm.ii.rr.aa.1282001@gmail.com>
2022-05-31 05:46:48 +0200

    First copy section three

---
+++ src/Component/ThirdSection.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import "../css/style.css";
+import bigImg from '../image/imgTwoSecThree.jpeg';
+import smallImg from '../image/imgOneSecThree.jpeg';
+class ThirdSection extends React.Component {
+    render() {
+        return ( 
+            <section className='thirdSec'>
+        <div className='container'>
+            <div className="parent">
+                <div className="div1">   <img src= "imgTwoSecThree.jpeg" className="bigImg"/></div>
+                <div className="div2">  <img src= "imgOneSecThree.jpeg" className="smallImg"/></div>
+                <div className="div3"> 
+                    <h2>COME & BE INSPIRED</h2>
+                    <P>We are excited to welcome you to our gallery and see how our collections influence you.</P>
+                </div>
+            </div>
+        </div>
+    </section>
+        )
+    }
+}
+export default ThirdSection;
